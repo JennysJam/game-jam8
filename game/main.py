771ee647d2986f439746782221f49bf7ba47202a
Jenny Dog <jenny.jam.internista@gmail.com>
2024-02-10 23:46:20 -0500

    added stubs

--- game/main.py
+++ game/main.py
@@ -1,5 +1,22 @@
-def main():
-    print("Hello, world!")
+import pygame
 
+window: pygame.Surface
+clock = pygame.time.Clock()
+def main():
+    init()
+    loop()
+
+def init():
+    global window
+    window = pygame.display.set_mode((300, 400))
+    window.fill('red')
+
+def loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        dt = clock.tick(60)
+        pygame.display.flip()
 
 main()
